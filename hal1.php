eda85b2b7901cfb8632f2d5362d9c18eca324e9d
dwiiiania <dwiayu248210@student.smkn1kandeman.sch.id>
2026-03-04 10:04:58 +0700

    hal1

---
+++ hal1.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1> hy watashi animek <h1>
+        <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi dolorem eligendi officia
+             sed velit dignissimos reprehenderit doloremque,
+             cupiditate sequi saepe maxime quaerat eum quae facere quas! Impedit corporis repellat incidunt.<p>
+</body>
+</html>
