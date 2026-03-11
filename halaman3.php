e2029e779fe4373599970fe593d432dda08454c0
Fina222 <nilazafina248222@student.smkn1kandeman.sch.id>
2026-03-11 09:59:57 +0700

    hal5

--- halaman3.php
+++ halaman3.php
@@ -5,9 +5,21 @@
     <meta name="viewport" content="width=
     , initial-scale=1.0">
     <title>Document</title>
+    <style>
+        body{
+            background : linear-gradient(to top right, #556E50, #A4D49A);
+        }
+        .container {
+            width : 1440px;
+            height : 1024px;
+            display : flex;
+            
+
+        }
+    </style>
 </head>
 <body>
     <h1>Halo Votly<h1>
         <p>selamat datang <p>
 </body>
-</html>
+</html> 
